5611172bc655f1f5c2bce1ac47122728418df93c
vackosar <admin@vaclavkosar.com>
2018-03-01 08:35:40 +0100

    impl auto redirect into add.sh

--- add.sh
+++ add.sh
@@ -1,20 +1,26 @@
-set -x
+#!/bin/sh
+set -xue
 
 title="$1"
-text="$2"
+link="$2"
 now="$(date +%F)"
 file="_posts/$now-$(echo "$title" | sed 's/ /-/g').md"
 
-if echo "$text" |grep -q "http"; then
-    text="[$text]($text)"
-fi
+#if echo "$text" |grep -q "http"; then
+#    link="$text"
+#    text="[$text]($text)"
+#fi
 
 echo "---
 layout: post
 title: \"$title\"
 date: $now
 ---
-$text" \
+[$link]($link)
+<script language=\"javascript\">
+    window.location.href = \"$link\"
+</script>
+"
 > "$file"
 
 git add "$file"
