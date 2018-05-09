5d0f6216b532de89a4fe223949a5a6eef467d149
vackosar <admin@vaclavkosar.com>
2018-05-09 19:58:57 +0200

    Auto push

--- add.sh
+++ add.sh
@@ -2,20 +2,20 @@
 set -xue
 
 title="$1"
-text="$2"
-now="$(date +%F)"
-file="_posts/$now-$(echo "$title" | sed 's/ /-/g').md"
+text="$2";
+now="$(date +%F)";
+file="_posts/$now-$(echo "$title" | sed 's/ /-/g').md";
 
 echo "---
 layout: post
 title: \"$title\"
 date: $now
 ---
-" > "$file"
+" > "$file";
 
 
 if expr match "$text" "^http" && expr match "$title" "^Link:"; then
-    link="$text"
+    link="$text";
     echo "---
     layout: post
     title: \"$title\"
@@ -25,11 +25,11 @@ if expr match "$text" "^http" && expr match "$title" "^Link:"; then
     <script language=\"javascript\">
         window.location.href = \"$link\"
     </script>
-    " >> "$file"
+    " >> "$file";
 else
-    echo "$text" >> "$file"
+    echo "$text" >> "$file";
 fi
 
 git add "$file"
 git commit -m "$title"
-echo "Please push"
+git push;
