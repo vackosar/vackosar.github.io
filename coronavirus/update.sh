a5a146e98d2a4ad39925625b2f3fbcff0d90065a
Vaclav Kosar <admin@vaclavkosar.com>
2020-04-10 07:58:44 +0200

    auto updates dates as well

--- coronavirus/update.sh
+++ coronavirus/update.sh
@@ -2,3 +2,5 @@
 
 wget -O time_series_covid19_confirmed_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
 wget -O time_series_covid19_deaths_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+sed -e "s/2020-04-09/$(date +%F)/g" templates/index.html > index.html;
+sed -e "s/2020-04-09/$(date +%F)/g" templates/sitemap.xml > sitemap.xml;
